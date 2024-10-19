acfdba1464b1582bf46df5dc39f3aa34b009d53a
Guten Ye <ywzhaifei@gmail.com>
2024-10-19 22:47:02 +0800

    feat: use pastel

--- src/cli/convert.ts
+++
@@ -1,5 +0,0 @@
-import { BitwardenToApplePasswords } from '#/converter'
-
-export function convert({ input, output }: { input: string; output: string }) {
-  return new BitwardenToApplePasswords().convert(input, output)
-}

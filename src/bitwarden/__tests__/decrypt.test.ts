ffd3edc4c19124cee7deb349b624f4774a6d5a75
Guten Ye <ywzhaifei@gmail.com>
2024-10-25 09:47:31 +0800

    encrypt: add

--- src/bitwarden/__tests__/decrypt.test.ts
+++
@@ -1,8 +0,0 @@
-import { expect, it } from 'bun:test'
-import { decrypt } from '../decrypt'
-
-it('decrypt', async () => {
-  const { fixtures } = globalThis.__TEST__
-  const received = await decrypt(fixtures.bitwardenEncrypted.data, '1')
-  expect(received).toEqual(fixtures.bitwardenDecrypted.data)
-})
---
+++ src/bitwarden/__tests__/encryptDecrypt.test.ts
@@ -0,0 +1,17 @@
+import { expect, it } from 'bun:test'
+import { decrypt, encrypt } from '../encryptDecrypt'
+
+const PASSWORD = '1'
+
+it('decrypt', async () => {
+  const { fixtures } = globalThis.__TEST__
+  const decryptedData = await decrypt(fixtures.bitwardenEncrypted.data, PASSWORD)
+  expect(decryptedData).toEqual(fixtures.bitwardenDecrypted.data)
+})
+
+it('encrypt', async () => {
+  const { fixtures } = globalThis.__TEST__
+  const encryptedData = await encrypt(fixtures.bitwardenDecrypted.data, PASSWORD)
+  const decryptedData = await decrypt(encryptedData, PASSWORD)
+  expect(decryptedData).toEqual(fixtures.bitwardenDecrypted.data)
+})
